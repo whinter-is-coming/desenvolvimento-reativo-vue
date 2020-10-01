d18d608a84291babdcf9a8583113eed68665f744
Whinter Goncalves <whinter.goncalves@gerencianet.com.br>
2020-10-01 09:36:02 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+   <h1>Conversor de moedas</h1>
+   <div class="linha">
+    <Conversor moeda-a="USD" moeda-b="BRL"></Conversor>
+    <Conversor moeda-a="BRL" moeda-b="USD"></Conversor>
+   </div>    
+
+   <div class="linha">
+    <Conversor moeda-a="CAD" moeda-b="BRL"></Conversor>
+    <Conversor moeda-a="BRL" moeda-b="CAD"></Conversor>
+   </div>
+
+   <div class="linha">
+    <Conversor moeda-a="EUR" moeda-b="BRL"></Conversor>
+    <Conversor moeda-a="BRL" moeda-b="EUR"></Conversor>
+   </div>
+
+  </div>
+</template>
+
+<script>
+import Conversor from './components/Conversor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Conversor
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 900px;
+  margin: auto;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.linha {
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-around;
+}
+
+</style>
